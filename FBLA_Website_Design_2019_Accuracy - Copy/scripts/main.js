eaaaffce53223405ce175834a746ef7c3204c83d
FishPasteMan <48975666+FishPasteMan@users.noreply.github.com>
2019-03-27 17:09:36 -0700

    Added Files

---
+++ FBLA_Website_Design_2019_Accuracy - Copy/scripts/main.js
@@ -0,0 +1,18 @@
+window.addEventListener('scroll', () => {
+    let parent = document.getElementById('parallax-container');
+    let children = parent.getElementsByTagName('div');
+    for(let i = 0; i < children.length; i++) {
+      children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
+    }
+ }, false)
+//don't touch that if you dont want all the stuff on all pages to break
+
+function mobileNavCreate(){
+  var bar = document.getElementById("mobileNavDrop");
+  if(bar.style.display === "block"){
+    bar.style.display = "none";
+  }
+  else{
+    bar.style.display = "block";
+  }
+}
